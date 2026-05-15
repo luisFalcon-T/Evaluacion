c7d3f233cd8055cf1d2571bd45114c888157e070
luisFalcon-T <luis_torrealbaf@icloud.com>
2026-05-15 10:47:10 -0400

    Avance

--- evaluacion/ejemploBd/GestionPreguntas.cs
+++ evaluacion/ejemploBd/GestionPreguntas.cs
@@ -9,15 +9,17 @@ namespace ejemploBd
 	public partial class GestionPreguntas : Form
 	{
 		// EXAMEN PASO 5.1: Esta cadena debe estar vacía. Cópiala de MainForm.cs
-		private string cadenaConexion = ""; 
+		private string cadenaConexion = "Server=localhost;Database=peducativa;Uid=root;Pwd=;"; 
 		private int _idModulo;
+		private string _nombreModulo;
 
 		// EXAMEN PASO 5.2: El constructor debe recibir el ID y el Nombre del módulo
-		public GestionPreguntas( _______ _____ ,____ _____ )
+		public GestionPreguntas(int id,string nombreModulo)
 		{
 			InitializeComponent();
 			// El estudiante debe añadir los parámetros y asignar los valores aquí
-			
+			this._idModulo = id;
+			this._nombreModulo = nombreModulo;
 			//
 			//lamar a funcion 
 		}
